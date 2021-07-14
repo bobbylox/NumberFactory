ac9ce2808f1b92bd9b1591e0670cd10cd4809b5c
Sakariye Abdi <smabdi@cps.edu>
2021-07-14 15:26:39 -0500

    Added movement to crane (left and down)
    When you press left, the crane goes left and when you press down, the crane goes down. I wasn't able to figure out how to make the crane go up just from making it go down.

--- Repo/NumberFactory/Assets/Scripts/CraneMovement.cs
+++ Repo/NumberFactory/Assets/Scripts/CraneMovement.cs
@@ -8,6 +8,8 @@ public class CraneMovement : MonoBehaviour
 
     bool moving_right;
     bool moving_left;
+    bool moving_up;
+    bool moving_down;
     public float column_distance = 2.0f;
     float speed = 0.2f;
     float target;
@@ -40,5 +42,53 @@ public class CraneMovement : MonoBehaviour
                 moving_right = false;
             }
         }
+
+
+        if (Input.GetKeyDown("left"))
+        {
+            moving_left = true;
+            target = transform.position.x - column_distance;
+        }
+
+
+
+        if (moving_left)
+        {
+            if (transform.position.x > target)
+            {
+                transform.Translate(new Vector3(Time.deltaTime * -speed, 0, 0));
+            }
+            else
+            {
+                moving_left = false;
+            }
+        }
+
+
+        if (Input.GetKeyDown("down"))
+        {
+            moving_down = true;
+            target = transform.position.y - column_distance;
+            moving_up = true;
+
+        }
+
+
+
+        if (moving_down)
+        {
+            if (transform.position.y > target)
+            {
+                transform.Translate(new Vector3(0, Time.deltaTime * -speed, 0));
+                //transform.Translate(new Vector3(0, Time.deltaTime * speed, 0));
+            }
+            else
+            {
+                moving_down = false;
+            }
+        }
+
+
+
     }
 }
