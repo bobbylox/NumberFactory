8e97701632f21f557b90ac4116f993fe6945a993
Sakariye Abdi <smabdi@cps.edu>
2021-08-10 15:32:24 -0500

    LevelSelectScene works perfectly

--- Repo/NumberFactory/Assets/Scripts/LevelSelect.cs
+++ Repo/NumberFactory/Assets/Scripts/LevelSelect.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class LevelSelect : MonoBehaviour
 {
-    public void LoadLevels (string levelName)
+    public LevelsManager levelsManager = new LevelsManager();
+    public void LoadLevels (int levelNumber)
     {
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene("GameScene");
+
+        string path = "Assets/LevelData/currentLevel.json";
+        levelsManager.currentLevel = levelNumber;
+        string currentLevelJSON = JsonUtility.ToJson(levelsManager);
+        File.WriteAllText(path, currentLevelJSON);
     }
 }
