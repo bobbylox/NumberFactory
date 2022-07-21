0d1dbe1345f7ab71b8bf9e256485c0793e410914
MaxwellWoodruff <40668505+MaxwellWoodruff@users.noreply.github.com>
2022-07-21 15:22:16 -0500

    blocks fall down when added

--- Repo/NumberFactory/Assets/Scripts/AdditionMachineScript.cs
+++ Repo/NumberFactory/Assets/Scripts/AdditionMachineScript.cs
@@ -11,6 +11,8 @@ public class AdditionMachineScript : MonoBehaviour
     public MachineScript machine;
     NumberScript number;
     NumberScript nextNumber;
+    public float gravity =-9.81f;
+    float velocity;
 
     public void Addition()
     {
@@ -18,19 +20,19 @@ public class AdditionMachineScript : MonoBehaviour
         {
             number = machine.inputs[0];
             nextNumber = machine.inputs[1];
-            number.transform.DOScale(0f, 0.5f).OnComplete(SwapNumber);
-            nextNumber.transform.DOScale(0f, 0.5f);
+            number.transform.DOMove(machine.gameObject.transform.position, 0.5f).OnComplete(SwapNumber);
+            nextNumber.transform.DOMove(machine.gameObject.transform.position, 0.5f);
             //Debug.Log("Addition");
         }
 
 
     }
-
+   
     void SwapNumber()
     {
         //Debug.Log("DEBUG " + number.number + ", " + nextNumber.GetStartScale());
         number.number = nextNumber.number + number.number;
-        number.transform.DOScale(nextNumber.GetStartScale(), 0.5f);
+        number.transform.DOMove(machine.slots[0].position, 0.5f);
         machine.inputs.Remove(nextNumber);
         Destroy(nextNumber.gameObject);
     }
