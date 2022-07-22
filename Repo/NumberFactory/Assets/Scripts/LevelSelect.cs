e2769fba34928affa11c2514264b85592435ef69
Rob Lockhart <bobbylox@gmail.com>
2022-07-22 15:36:20 -0500

    "ShowAllLevels" toggle in Unity Editor now unlocks/locks buttons past currentLevel

--- Repo/NumberFactory/Assets/Scripts/LevelSelect.cs
+++ Repo/NumberFactory/Assets/Scripts/LevelSelect.cs
@@ -11,6 +11,9 @@ public class LevelSelect : MonoBehaviour
     public LevelsManager levelsManager;
     public int currentLevel;
     string pathToCurrentLevel = "Assets/LevelData/currentLevel.json";
+    public bool showAllLevels = false;
+    //[SerializeField]
+    private bool showingAllLevels = false;
 
     void Start()
     {
@@ -37,5 +40,32 @@ public class LevelSelect : MonoBehaviour
         levelsManager.currentLevel = levelNumber;
         string currentLevelJSON = JsonUtility.ToJson(levelsManager);
         File.WriteAllText(path, currentLevelJSON);
+    }
+
+    void Update()
+    {
+        //toggling "showAllLevels" in Unity Editor should enable/disable level buttons beyond currentLevel
+        if(showAllLevels && !showingAllLevels)
+        {
+            showingAllLevels = true;
+
+            foreach (Button levelButton in levelButtons)
+            {
+                levelButton.interactable = true;
+            }
+        }
+        else if ( !showAllLevels && showingAllLevels)
+        {
+            showingAllLevels = false;
+
+            foreach (Button levelButton in levelButtons)
+            {
+                if (int.Parse(levelButton.gameObject.name) > currentLevel)
+                {
+                    levelButton.interactable = false;
+                    //devloper mode change false to true.
+                }
+            }
+        }
     }
 }
