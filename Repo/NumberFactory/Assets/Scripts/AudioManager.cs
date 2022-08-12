64112bf0ad3fac4dc49bc07ff40b40b30836816f
MaxwellWoodruff <40668505+MaxwellWoodruff@users.noreply.github.com>
2022-08-12 16:05:57 -0500

    AudioManger and music

---
+++ Repo/NumberFactory/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sounds[] sounds;
+
+
+    void Awake()
+    {
+        foreach (Sounds s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+
+           s.source.volume = s.volume;
+           s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play (string name)
+    {
+       Sounds s = Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+    }
+}
